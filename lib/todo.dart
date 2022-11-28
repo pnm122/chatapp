a72b8932b9d6e0d956c86625643a06692061ead5
Pierce Martin <pnm12@pitt.edu>
2022-11-28 01:32:41 -0500

    Hosting files added

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,14 +1,20 @@
 class Todo {
+  // 2.0.0:
+  //   TODO: Google authentication
+  //   TODO: Create groups with password
+  //   TODO: Show the list of groups you're in and be able to select between them
+
   // TODO: Don't allow users to sign in as another past user (username and password?)
   // TODO: Dark mode switiching support
   // TODO: Replies (using gestures? :o)
   // TODO: Attachments
-  // TODO: Pause autoscrolling when user is scrolled up looking at messages; give them a button to jump back and alerts for new messages
   // TODO: Notification alert
   // TODO: Different sign in method for better sign in/out behavior
-  // TODO: Nicer colors
-  // TODO: Cursor change on hover for sending a message
   // TODO: Tell when a person is in the browser? Or when they go AFK?
+  // TODO: Get enter key to work
+  // TODO: Font on mobile browser?
+  // TODO: Breaks when you leave the site and then open it again
+  // TODO: Group together messages in similar times
   
   
   // Semi-solved: Scroll to bottom when chat room created and on message sent and message receieved
